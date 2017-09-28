4742675644013058a0d61b69ae2f477d283bf3d6
Ivan Bogdev <ivan.bogdev@gmail.com>
2017-09-28 12:23:42 -0400

    login - added front end error messsage
    #7 error msg for bad login

--- soen343app/public/login/login.js
+++ soen343app/public/login/login.js
@@ -2,6 +2,8 @@ var loginModule = angular.module('myApp.login', []);
 
 loginModule.controller('LoginController', ['$scope', '$location', 'Authentication', "$http", "$cookies",
 function loginController($scope, $location, Authentication, $http, $cookies) {
+   $scope.errorMsg = "";
+
    $scope.login = function() {
       Authentication.login($scope.email, $scope.password, function() {
          /*
@@ -14,10 +16,16 @@ function loginController($scope, $location, Authentication, $http, $cookies) {
          }
          $http.post(url,data)
          .then((res) => {
-            $scope.userInfo = res.data;
-            $cookies.putObject('userInfo', $scope.userInfo);
-            console.log("Successful login for: ",$scope.userInfo.email);
-            $location.path('/');
+            if(res.data == "Wrong Password" || res.data == "Email not found"){
+               $scope.errorMsg = res.data;
+               return;
+            }
+            else {
+               $scope.userInfo = res.data;
+               $cookies.putObject('userInfo', $scope.userInfo);
+               console.log("Successful login for: ",$scope.userInfo.email);
+               $location.path('/');
+            }
          });
       });
    };
