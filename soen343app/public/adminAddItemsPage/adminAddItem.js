85f751f64b82a64fc15ee32c9ced503207dccc5f
Martin <martinas95@yahoo.fr>
2017-09-29 03:10:14 -0400

    fixed front end issues

--- soen343app/public/adminAddItemsPage/adminAddItem.js
+++ soen343app/public/adminAddItemsPage/adminAddItem.js
@@ -16,4 +16,65 @@ angular.module('myApp.adminAddItem', ['ngCookies'])
          $location.path('/');
          return;
       }
+      $scope.addTablet = function(){
+            var url = "/post/addItem";
+            var data = {
+              type : "tablet",
+              model : $scope.modelNumber,
+              weight : $scope.weight,
+              price : $scope.price,
+              brand : $scope.brand,
+              processorType : $scope.processor,
+              cpuCores : $scope.cores, 
+              ram : $scope.ram,
+              hardDriveSize : $scope.hardDriveSize,
+              dimension : $scope.dimensions,
+              batteryInfo : $scope.battery,
+              operatingSystem : $scope.operatingSystem,
+              cameraInfo : $scope.camera
+            }
+            $http.post(url,data)
+            .then((res) => {
+               });
+      }
+      $scope.addDesktop = function(){
+            var url = "/post/addItem";
+            var data = {
+              type : "desktop",
+              model : $scope.modelNumber,
+              weight : $scope.weight,
+              price : $scope.price,
+              brand : $scope.brand,
+              processorType : $scope.processor,
+              cpuCores : $scope.cpuCores, 
+              ram : $scope.ram,
+              hardDriveSize : $scope.hardDriveSize,
+              dimension : $scope.dimensions,
+            }
+            $http.post(url,data)
+            .then((res) => {
+               });
+      }
+      $scope.addLaptop = function(){
+            var url = "/post/addItem";
+            var data = {
+              type : "laptop",
+              model : $scope.model,
+              weight : $scope.weight,
+              price : $scope.price,
+              brand : $scope.brand,
+              processorType : $scope.processor,
+              cpuCores : $scope.cors, 
+              ram : $scope.ram,
+              hardDriveSize : $scope.hardDriveSize,
+              dimension : $scope.dimensions,
+              batteryInfo : $scope.battery,
+              operatingSystem : $scope.operatingSystem,
+              cameraInfo : $scope.camera,
+              touchScreen : $scope.touchScreen
+            }
+            $http.post(url,data)
+            .then((res) => {
+               });
+      }
 }]);
