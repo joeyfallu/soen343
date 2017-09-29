2fca6ebb47e3d1e380188dd3df7d75e195cdaefa
claudiaconfalone <confaloneclaudia@gmail.com>
2017-09-29 04:32:41 -0400

    fixed variables

--- soen343app/public/adminAddItemsPage/adminAddItem.js
+++ soen343app/public/adminAddItemsPage/adminAddItem.js
@@ -20,11 +20,11 @@ angular.module('myApp.adminAddItem', ['ngCookies'])
             var url = "/post/addItem";
             var data = {
               type : "monitor",
-              model : $scope.modelNumber,
-              weight : $scope.weight,
-              price : $scope.price,
-              brand : $scope.brand,
-              size : $scope.size
+              model : $scope.monitorModelNumber,
+              weight : $scope.monitorWeight,
+              price : $scope.monitorPrice,
+              brand : $scope.monitorBrand,
+              size : $scope.monitorSize
             }
             $http.post(url,data)
             .then((res) => {
@@ -35,11 +35,11 @@ angular.module('myApp.adminAddItem', ['ngCookies'])
             var url = "/post/addItem";
             var data = {
               type : "television",
-              model : $scope.modelNumber,
-              weight : $scope.weight,
-              price : $scope.price,
-              brand : $scope.brand,
-              dimensions : $scope.dimensions
+              model : $scope.tvModelNumber,
+              weight : $scope.tvWeight,
+              price : $scope.tvPrice,
+              brand : $scope.tvBrand,
+              dimensions : $scope.tvDimensions
             }
             $http.post(url,data)
             .then((res) => {
@@ -50,18 +50,19 @@ angular.module('myApp.adminAddItem', ['ngCookies'])
             var url = "/post/addItem";
             var data = {
               type : "tablet",
-              model : $scope.modelNumber,
-              weight : $scope.weight,
-              price : $scope.price,
-              brand : $scope.brand,
-              processorType : $scope.processor,
-              cpuCores : $scope.cores, 
-              ram : $scope.ram,
-              hardDriveSize : $scope.hardDriveSize,
-              dimension : $scope.dimensions,
-              batteryInfo : $scope.battery,
-              operatingSystem : $scope.operatingSystem,
-              cameraInfo : $scope.camera
+              model : $scope.tabletModelNumber,
+              weight : $scope.tabletWeight,
+              price : $scope.tabletPrice,
+              brand : $scope.tabletBrand,
+              processorType : $scope.tabletProcessor,
+              cpuCores : $scope.tabletCores,
+              ram : $scope.tabletRam,
+              hardDriveSize : $scope.tabletHardDriveSize,
+              dimension : $scope.tabletDimensions,
+              batteryInfo : $scope.tabletBattery,
+              operatingSystem : $scope.tabletOperatingSystem,
+              cameraInfo : $scope.tabletCamera,
+
             }
             $http.post(url,data)
             .then((res) => {
@@ -71,15 +72,15 @@ angular.module('myApp.adminAddItem', ['ngCookies'])
             var url = "/post/addItem";
             var data = {
               type : "desktop",
-              model : $scope.modelNumber,
-              weight : $scope.weight,
-              price : $scope.price,
-              brand : $scope.brand,
-              processorType : $scope.processor,
-              cpuCores : $scope.cpuCores, 
-              ram : $scope.ram,
-              hardDriveSize : $scope.hardDriveSize,
-              dimension : $scope.dimensions
+              model : $scope.desktopModelNumber,
+              weight : $scope.desktopWeight,
+              price : $scope.desktopPrice,
+              brand : $scope.desktopBrand,
+              processorType : $scope.desktopProcessor,
+              cpuCores : $scope.desktopCpuCores,
+              ram : $scope.desktopRam,
+              hardDriveSize : $scope.desktopHardDriveSize,
+              dimension : $scope.desktopDimensions
             }
             $http.post(url,data)
             .then((res) => {
@@ -89,19 +90,19 @@ angular.module('myApp.adminAddItem', ['ngCookies'])
             var url = "/post/addItem";
             var data = {
               type : "laptop",
-              model : $scope.model,
-              weight : $scope.weight,
-              price : $scope.price,
-              brand : $scope.brand,
-              processorType : $scope.processor,
-              cpuCores : $scope.cors, 
-              ram : $scope.ram,
-              hardDriveSize : $scope.hardDriveSize,
-              dimension : $scope.dimensions,
-              batteryInfo : $scope.battery,
-              operatingSystem : $scope.operatingSystem,
-              cameraInfo : $scope.camera,
-              touchScreen : $scope.touchScreen
+              model : $scope.laptopModel,
+              weight : $scope.laptopWeight,
+              price : $scope.laptopPrice,
+              brand : $scope.laptopBrand,
+              processorType : $scope.laptopProcessor,
+              cpuCores : $scope.laptopCores,
+              ram : $scope.laptopRam,
+              hardDriveSize : $scope.laptopHardDriveSize,
+              dimension : $scope.laptopDimensions,
+              batteryInfo : $scope.laptopBattery,
+              operatingSystem : $scope.laptopOperatingSystem,
+              cameraInfo : $scope.laptopCamera,
+              touchScreen : $scope.laptopTouchScreen
             }
             $http.post(url,data)
             .then((res) => {
