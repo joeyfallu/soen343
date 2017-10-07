0b68d08ccd927339f8e6091b82c78572b3cebbc4
SaiShan <saishan.ry@gmail.com>
2017-10-07 14:11:29 -0400

    add methods in Transaction class

--- src/main/java/com/example/myapp/Transaction.java
+++ src/main/java/com/example/myapp/Transaction.java
@@ -3,8 +3,16 @@ package com.example.myapp;
 public class Transaction {
     boolean isComplete;
 
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
     void becomeComplete(){
 
+    }
+
+    public Transaction(boolean isComplete) {
+        this.isComplete = isComplete;
     }
 
     boolean isComplete(){
