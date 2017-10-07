e860085470522ada72d9de9a163e760b90b22ea1
SaiShan <saishan.ry@gmail.com>
2017-10-07 14:08:52 -0400

    ProductEntry and Transaction classes

--- src/main/java/com/example/myapp/ProductEntry.java
+++ src/main/java/com/example/myapp/ProductEntry.java
@@ -1,6 +1,17 @@
 package com.example.myapp;
 
-public class ProductEntry {
+public class ProductEntry extends Transaction {
 
 
+    void addNewProduct(Product p){
+
+    }
+
+    void deleteProduct(ProductId id){
+
+    }
+
+    void modifyProduct (Product p, ProductId id){
+
+    }
 }
---
+++ src/main/java/com/example/myapp/Transaction.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+public class Transaction {
+    boolean isComplete;
+
+    void becomeComplete(){
+
+    }
+
+    boolean isComplete(){
+        boolean complete = false;
+
+        return complete;
+    }
+}
