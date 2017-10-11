755cd5c699a0d78fbb17b033efa38f9ae653e257
Philippe Li <philo1196@gmail.com>
2017-10-11 14:14:40 -0400

    wip #45 started admin delete items form

---
+++ src/main/resources/static/js/adminDeleteItems/adminDeleteItems.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var adminDeleteItemsModule = angular.module('myApp.adminDeleteItems', ['ngRoute']);
+
+adminDeleteItemsModule.controller('AdminDeleteItemsController', [function adminDeleteItemsController() {
+
+}]);
+
