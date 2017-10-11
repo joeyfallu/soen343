14dfc2d8ac4beaf065951f2636295b6c87e97eaf
Ivan Bogdev <ivan.bogdev@gmail.com>
2017-10-11 13:21:08 -0400

    modify template logic

--- src/main/resources/static/js/modifyItem.js
+++ src/main/resources/static/js/modifyItem.js
@@ -3,13 +3,13 @@
 var app = angular.module('modifyItem', []);
 app.controller('modifyItemCtrl', function($scope, $http){
 
-    var id;     //TODO: initialize id from url
-    var urlProduct = '/getItem/'; //TODO: ADD item ID to request
-        $scope.item = [];
+    var id = 6;     //TODO: initialize id from url
+    var urlProduct = '/getItem/' + id;
+    $scope.itemData = [];
 
-        $http.get(urlProduct)
-        .then((res) => {
-          $scope.itemData = res.data
-          console.log($scope.itemData);
+    $http.get(urlProduct)
+    .then((res) => {
+        $scope.itemData = res.data;
+        console.log($scope.itemData);
         });
 });
