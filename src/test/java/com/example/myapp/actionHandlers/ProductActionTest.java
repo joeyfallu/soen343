d42df47a30d5144745b230b30e35d1816f586709
Zachary Kogan <zackkogan@hotmail.com>
2017-10-11 22:39:54 -0400

    Added ProductActionTest Class

---
+++ src/test/java/com/example/myapp/actionHandlers/ProductActionTest.java
@@ -0,0 +1,30 @@
+package com.example.myapp.actionHandlers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductActionTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addNewProduct() throws Exception {
+    }
+
+    @Test
+    public void deleteProduct() throws Exception {
+    }
+
+    @Test
+    public void modifyProduct() throws Exception {
+    }
+
+}
