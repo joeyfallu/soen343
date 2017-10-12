58b75282a96024c7ec8d0c777f3a14cc6e47c84f
Zachary Kogan <zackkogan@hotmail.com>
2017-10-11 22:43:21 -0400

    Added ActionTest Class

---
+++ src/test/java/com/example/myapp/actionHandlers/ActionTest.java
@@ -0,0 +1,26 @@
+package com.example.myapp.actionHandlers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ActionTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void isComplete() throws Exception {
+    }
+
+    @Test
+    public void becomeComplete() throws Exception {
+    }
+
+}
