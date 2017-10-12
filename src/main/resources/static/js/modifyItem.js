82258432122228b651a7267d016940c82954714e
Ivan Bogdev <ivan.bogdev@gmail.com>
2017-10-12 10:14:00 -0400

    fixed code placement
    scope functions should be outside the http.post request

--- src/main/resources/static/js/modifyItem.js
+++ src/main/resources/static/js/modifyItem.js
@@ -3,6 +3,103 @@
 var app = angular.module('modifyItem', []);
 app.controller('modifyItemCtrl', function($scope, $http){
 
+    $scope.modifyTV = function(){
+        var url ="/post/modify";
+        var data = {
+            id : itemData.id,
+            discriminator : "1",
+            model : $scope.itemData.model,
+            weight : $scope.itemData.weight,
+            price : $scope.itemData.price,
+            brand : $scope.itemData.brand,
+            dimensions : $scope.itemData.dimensions,
+        }
+        $http.post(url,data)
+        .then((res) => {
+        });
+     };
+    $scope.modifyMonitor = function(){
+          var url = "/post/modify";
+          var data = {
+            id : itemData.id,
+            discriminator : "2",
+            model : $scope.itemData.model,
+            weight : $scope.itemData.weight,
+            price : $scope.itemData.price,
+            brand : $scope.itemData.brand,
+            size : $scope.itemData.size,
+          }
+          $http.post(url,data)
+          .then((res) => {
+          });
+    };
+    $scope.modifyTablet = function(){
+            var url = "/post/modify";
+            var data = {
+                id : itemData.id,
+                discriminator : "3",
+                model : $scope.itemData.model,
+                weight : $scope.itemData.weight,
+                price : $scope.itemData.price,
+                brand : $scope.itemData.brand,
+                dimensions : $scope.itemData.dimensions,
+                processorType : $scope.itemData.processorType,
+                cpuCores : $scope.itemData.cpuCores,
+                ram : $scope.itemData.ram,
+                hardDriveSize : $scope.itemData.hardDriveSize,
+                size : $scope.itemData.size,
+                batteryInfo : $scope.itemData.batteryInfo,
+                operatingSystem : $scope.itemData.operatingSystem,
+                cameraInfo : $scope.itemData.cameraInfo,
+            }
+            $http.post(url,data)
+            .then((res) => {
+            });
+    };
+    $scope.modifyDesktop = function(){
+            var url = "/post/modify";
+            var data = {
+                id : itemData.id,
+                discriminator : "4",
+                model : $scope.itemData.model,
+                weight : $scope.itemData.weight,
+                price : $scope.itemData.price,
+                brand : $scope.itemData.brand,
+                dimensions : $scope.itemData.dimensions,
+                processorType : $scope.itemData.processorType,
+                cpuCores : $scope.itemData.cpuCores,
+                ram : $scope.itemData.ram,
+                hardDriveSize : $scope.itemData.hardDriveSize,
+                }
+            $http.post(url,data)
+            .then((res) => {
+            });
+    };
+    $scope.modifyLaptop = function(){
+        var url = "/post/modify";
+        var data = {
+            id : itemData.id,
+            discriminator : "5",
+            model : $scope.itemData.model,
+            weight : $scope.itemData.weight,
+            price : $scope.itemData.price,
+            brand : $scope.itemData.brand,
+            processorType : $scope.itemData.processorType,
+            cpuCores : $scope.itemData.cpuCores,
+            ram : $scope.itemData.ram,
+            hardDriveSize : $scope.itemData.hardDriveSize,
+            size : $scope.itemData.size,
+            batteryInfo : $scope.itemData.batteryInfo,
+            operatingSystem : $scope.itemData.operatingSystem,
+            cameraInfo : $scope.itemData.cameraInfo,
+            touchScreen : $scope.itemData.touchScreen,
+        }
+        $http.post(url,data)
+        .then((res) => {
+        });
+    };
+
+
     var id = 6;     //TODO: initialize id from url
     var urlProduct = '/getItem/' + id;
     $scope.itemData = [];
@@ -12,108 +109,6 @@ app.controller('modifyItemCtrl', function($scope, $http){
         $scope.itemData = res.data;
         console.log($scope.itemData);
         });
-
-
-       $scope.modifyTV = function(){
-           var url ="/post/modify";
-           var data = {
-               id : itemData.id,
-               discriminator : "1",
-               model : $scope.itemData.model,
-               weight : $scope.itemData.weight,
-               price : $scope.itemData.price,
-               brand : $scope.itemData.brand,
-               dimensions : $scope.itemData.dimensions,
-           }
-           $http.post(url,data)
-           .then((res) => {
-            });
-        };
-
-        $scope.modifyMonitor = function(){
-            var url = "/post/modify";
-            var data = {
-                id : itemData.id,
-                discriminator : "2",
-                model : $scope.itemData.model,
-                weight : $scope.itemData.weight,
-                price : $scope.itemData.price,
-                brand : $scope.itemData.brand,
-                size : $scope.itemData.size,
-            }
-            $http.post(url,data)
-            .then((res) => {
-            });
-         };
-
-
-        $scope.modifyTablet = function(){
-            var url = "/post/modify";
-            var data = {
-                id : itemData.id,
-                discriminator : "3",
-                model : $scope.itemData.model,
-                weight : $scope.itemData.weight,
-                price : $scope.itemData.price,
-                brand : $scope.itemData.brand,
-                 dimensions : $scope.itemData.dimensions,
-                 processorType : $scope.itemData.processorType,
-                 cpuCores : $scope.itemData.cpuCores,
-                 ram : $scope.itemData.ram,
-                 hardDriveSize : $scope.itemData.hardDriveSize,
-                 size : $scope.itemData.size,
-                 batteryInfo : $scope.itemData.batteryInfo,
-                 operatingSystem : $scope.itemData.operatingSystem,
-                 cameraInfo : $scope.itemData.cameraInfo,
-            }
-            $http.post(url,data)
-            .then((res) => {
-            });
-         };
-
-         $scope.modifyDesktop = function(){
-         var url = "/post/modify";
-         var data = {
-            id : itemData.id,
-            discriminator : "4",
-            model : $scope.itemData.model,
-            weight : $scope.itemData.weight,
-            price : $scope.itemData.price,
-            brand : $scope.itemData.brand,
-            dimensions : $scope.itemData.dimensions,
-            processorType : $scope.itemData.processorType,
-            cpuCores : $scope.itemData.cpuCores,
-            ram : $scope.itemData.ram,
-            hardDriveSize : $scope.itemData.hardDriveSize,
-         }
-         $http.post(url,data)
-         .then((res) => {
-          });
-         };
-
-         $scope.modifyLaptop = function(){
-         var url = "/post/modify";
-         var data = {
-             id : itemData.id,
-             discriminator : "5",
-             model : $scope.itemData.model,
-             weight : $scope.itemData.weight,
-             price : $scope.itemData.price,
-             brand : $scope.itemData.brand,
-             processorType : $scope.itemData.processorType,
-             cpuCores : $scope.itemData.cpuCores,
-             ram : $scope.itemData.ram,
-             hardDriveSize : $scope.itemData.hardDriveSize,
-             size : $scope.itemData.size,
-             batteryInfo : $scope.itemData.batteryInfo,
-             operatingSystem : $scope.itemData.operatingSystem,
-             cameraInfo : $scope.itemData.cameraInfo,
-             touchScreen : $scope.itemData.touchScreen,
-         }
-         $http.post(url,data)
-         .then((res) => {
-          });
-         };
 
 
 });
