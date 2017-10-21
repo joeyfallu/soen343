8ada642e5e6381ae6f7579fb0a271f763d35ca62
Philippe Li <philo1196@gmail.com>
2017-10-21 17:02:38 -0400

    wip #33 testing single page app

--- src/main/java/com/example/myapp/Routing.java
+++ src/main/java/com/example/myapp/Routing.java
@@ -7,54 +7,60 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Routing {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String getHomeTemplate() {
-        return "index";
-    }
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    public String getHomeTemplate() {
+//        return "index";
+//    }
+//
+//    @RequestMapping(value = "/login", method = RequestMethod.GET)
+//    public String getLoginTemplate() {
+//        return "login";
+//    }
+//
+//    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+//    public String getAdminPageTemplate() {
+//        return "admin/admin";
+//    }
+//
+//    @RequestMapping(value="/addItems", method = RequestMethod.GET)
+//    public String getAddItemsTemplate() {
+//        return "admin/addItems";
+//    }
+//
+//    @RequestMapping(value="/viewItems", method = RequestMethod.GET)
+//    public String getViewItemsTemplate(){
+//        return "admin/viewItems";
+//    }
+//
+//    @RequestMapping(value="/modifyItems", method = RequestMethod.GET)
+//    public String getModifyItemsTemplate(){
+//        return "admin/modifyItems";
+//    }
+//
+//    @RequestMapping(value="/deleteItems", method = RequestMethod.GET)
+//    public String getDeleteItemsTemplate(){
+//        return "admin/deleteItems";
+//    }
+//
+//    @RequestMapping(value = "/registerAdmin", method = RequestMethod.GET)
+//    public String getRegisterAdminTemplate() {
+//        return "registerAdmin";
+//    }
+//
+//    @RequestMapping(value="/addUsers", method = RequestMethod.GET)
+//    public String getAddUsersTemplate() {
+//        return "admin/addUsers";
+//    }
+//
+//    @RequestMapping(value = "/test", method = RequestMethod.GET)
+//    public String getTestPageTemplate() {
+//        return "static/testPage";
+//    }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String getLoginTemplate() {
-        return "login";
-    }
-
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String getAdminPageTemplate() {
-        return "admin/admin";
-    }
-
-    @RequestMapping(value="/addItems", method = RequestMethod.GET)
-    public String getAddItemsTemplate() {
-        return "admin/addItems";
-    }
-
-    @RequestMapping(value="/viewItems", method = RequestMethod.GET)
-    public String getViewItemsTemplate(){
-        return "admin/viewItems";
-    }
-
-    @RequestMapping(value="/modifyItems", method = RequestMethod.GET)
-    public String getModifyItemsTemplate(){
-        return "admin/modifyItems";
-    }
-
-    @RequestMapping(value="/deleteItems", method = RequestMethod.GET)
-    public String getDeleteItemsTemplate(){
-        return "admin/deleteItems";
-    }
-
-    @RequestMapping(value = "/registerAdmin", method = RequestMethod.GET)
-    public String getRegisterAdminTemplate() {
-        return "registerAdmin";
-    }
-
-    @RequestMapping(value="/addUsers", method = RequestMethod.GET)
-    public String getAddUsersTemplate() {
-        return "admin/addUsers";
-    }
-
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String getTestPageTemplate() {
-        return "testPage";
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirect() {
+        // Forward to home page so that route is preserved.
+        return "forward:/";
     }
 
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,16 @@
 'use strict';
 
-const indexModule = angular.module('index', ['ngAnimate']);
+const appModule = angular.module('app', ['ngRoute']);
 
-indexModule.controller("indexController", function indexController($scope) {
+// indexModule.controller("indexController", function indexController($scope) {
+//
+// });
 
+appModule.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+        .when('/', { templateUrl: "view/frontPage.html"})
+        .when('/test', { templateUrl: "view/testPage.html" })
+        .otherwise({ redirectTo: '/'});
 });
