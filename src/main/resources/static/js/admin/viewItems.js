ab33f479725d9a85dc3822e48d822abc9b1ae6a7
Kabellard <Keven_abellard@hotmail.com>
2017-10-27 01:17:51 -0400

    Added working filtering for brand filter, model filter and type filter. Issues to fix with filtering by size

--- src/main/resources/static/js/admin/viewItems.js
+++ src/main/resources/static/js/admin/viewItems.js
@@ -15,40 +15,62 @@ angular.module('app').controller('viewItemsCtrl', function ($scope, $http) {
 
         $scope.itemsInventory = items;
         $scope.select = null;
+        $scope.filterSelect = null;
 
-        $scope.listOfOptions = ['Most Expensive', 'Least Expensive', 'Most Recent', 'Least Recent', 'Heaviest', 'Lightest', 'Brand (A-Z)', 'Brand (Z-A)'];
+        $scope.listOfSorts = ['Most Expensive', 'Least Expensive', 'Most Recent', 'least Recent', 'Heaviest', 'lightest', 'Brand (A to z)', 'Brand (z to A)', 'By type'];
 
+
+        
         $scope.rename = function(x){
-                   if (x === 'Most Expensive' ){
-                       $scope.sortBy('-price');
-                   }
-                   if (x === 'Least Expensive' ){
-                       $scope.sortBy('price');
-                   }
-                   if (x === 'Most Recent' ){
-                       $scope.sortBy('-id');
-                   }
-                   if (x === 'Least Recent' ){
-                       $scope.sortBy('id');
-                   }
-                   if (x === 'Heaviest' ){
-                       $scope.sortBy('-weight');
-                   }
-                   if (x === 'Lightest' ){
-                       $scope.sortBy('weight');
-                   }
-                   if (x === 'Brand (A-Z)' ){
-                       $scope.sortBy('brand');
-                   }
-                   if (x === 'Brand (Z-A)' ){
-                       $scope.sortBy('-brand');
-                   }
+            if (x === 'Most Expensive' ){
+                $scope.sortBy('-price');
+            }
+            if (x === 'Least Expensive' ){
+                $scope.sortBy('price');
+            }
+            if (x === 'Most Recent' ){
+                $scope.sortBy('-id');
+            }
+            if (x === 'least Recent' ){
+                $scope.sortBy('id');
+            }
+            if (x === 'Heaviest' ){
+                $scope.sortBy('-weight');
+            }
+            if (x === 'lightest' ){
+                $scope.sortBy('weight');
+            }
+            if (x === 'Brand (A to z)' ){
+                $scope.sortBy('brand');
+            }
+            if (x === 'Brand (z to A)' ){
+                $scope.sortBy('-brand');
+            }
+            if (x === 'By type'){
+                $scope.sortBy('discriminator');
+            } 
+        }
 
-               }
+
 
         $scope.sortBy = function(select) {
             $scope.select = select;
-          };
+        };
+
+        //https://stackoverflow.com/questions/24081004/angularjs-ng-repeat-filter-when-value-is-greater-than
+        $scope.greaterThan = function(prop, val){
+            return function(item){
+              return item[prop] > val;
+            }
+        }
+
+        $scope.SmallerThan = function(prop, val){
+            return function(item){
+              return item[prop] > val;
+            }
+        }
+
+
 
         console.log($scope.itemsInventory);
     }).catch((err) => {
