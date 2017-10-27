a6314b7af9021201fcbf419846cd327e17dec93a
Ivan Bogdev <ivan.bogdev@gmail.com>
2017-10-26 22:11:45 -0400

    removed useless logging console.log

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -12,12 +12,8 @@ angular.module('app')
                 email: $scope.email,
                 password: $scope.password
             };
-
-            console.log("attempting login POST request...");
             $scope.errorMsg = "";
             $http.post(url, data).then((res) => {
-                console.log("receiving response from login POST request:")
-                console.log(res);
                 console.log(res.data);
                 if (res.data.message) {
                     $scope.errorMsg = res.data.message;
