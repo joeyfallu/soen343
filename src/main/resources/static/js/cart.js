6d96ba5fdb3ce8e08b80ddc15d3b4c7685abd82c
mateuse <emateus71@gmail.com>
2017-11-05 15:32:19 -0500

    Allow pom.xml to be updated

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('app')
-    .controller('cartController', function () {
+    .controller('cartController', function ($scope, $http, $cookies) {
+
+        $scope.cart = "";
+        const urlCart = '/get/products';
+
+         $http.get(urlCart).then((res) => {
+
+            $scope.cart = res.data;
+         });
 
     });
