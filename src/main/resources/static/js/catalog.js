dc2317730a2d9d99541439a6dd0e4cc25cb16208
mateuse <emateus71@gmail.com>
2017-11-07 01:03:52 -0500

    Most cart functionality including, adding/removing/purchasing from cart, timeout function,seperate pages for each type of product.
    Missing the lock on a item when it is added to cart for now

--- src/main/resources/static/js/catalog.js
+++ src/main/resources/static/js/catalog.js
@@ -1,6 +1,37 @@
 'use strict';
 
 angular.module('app')
-    .controller('catalogController', function () {
+    .controller('catalogController', function ($scope,$http, $cookies) {
+
+
+        $scope.itemsInventory = "";
+        const urlProduct = '/get/products';
+
+        $http.get(urlProduct).then((res) => {
+
+            $scope.itemsInventory = res.data;
+            console.log($scope.itemsInventory);
+        });
+
+        $scope.addToCart = function(itemId){
+            const addToCartUrl = '/post/addToCart';
+            $http.post(addToCartUrl, itemId).then((res) => {
+                if(res.data.message == "Too many items in the cart"){
+                    alert("There are too many items in the cart\nMax items is 7 this item was not added");
+                }
+                else{
+                    var expireDate = new Date();
+                    //5 seconds * 1000 milliseconds
+                    expireDate.setTime(expireDate.getTime() + (300*1000));
+
+                    $cookies.put(itemId, itemId, {'expires': expireDate})
+
+                    delete $scope.itemsInventory[itemId];
+                }
+            }).catch((err) => {
+                console.log("ERROR:");
+                console.log(err);
+            });
+        }
 
     });
