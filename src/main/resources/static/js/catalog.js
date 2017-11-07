f6a94195f42785506b8ea770ebedb97bd385580c
Kabellard <Keven_abellard@hotmail.com>
2017-11-07 18:09:02 -0500

    added sorting to new catalog

--- src/main/resources/static/js/catalog.js
+++ src/main/resources/static/js/catalog.js
@@ -8,8 +8,52 @@ angular.module('app')
         const urlProduct = '/get/products';
 
         $http.get(urlProduct).then((res) => {
-
+            
             $scope.itemsInventory = res.data;
+
+            var items = [];
+            for(var key in res.data){
+                items.push(res.data[key]);
+            }
+
+            $scope.itemsInventory = items;
+            $scope.select = null;
+            $scope.search3= 0;
+            $scope.search4 = 100000000;
+
+            $scope.listOfOptions = ['Most Expensive', 'Least Expensive', 'Most Recent', 'Least Recent', 'Heaviest', 'Lightest', 'Brand (A-Z)', 'Brand (Z-A)'];
+            
+                    $scope.rename = function(x){
+                               if (x === 'Most Expensive' ){
+                                   $scope.sortBy('-price');
+                               }
+                               if (x === 'Least Expensive' ){
+                                   $scope.sortBy('price');
+                               }
+                               if (x === 'Most Recent' ){
+                                   $scope.sortBy('-id');
+                               }
+                               if (x === 'Least Recent' ){
+                                   $scope.sortBy('id');
+                               }
+                               if (x === 'Heaviest' ){
+                                   $scope.sortBy('-weight');
+                               }
+                               if (x === 'Lightest' ){
+                                   $scope.sortBy('weight');
+                               }
+                               if (x === 'Brand (A-Z)' ){
+                                   $scope.sortBy('brand');
+                               }
+                               if (x === 'Brand (Z-A)' ){
+                                   $scope.sortBy('-brand');
+                               }
+                            }
+
+                            $scope.sortBy = function(select) {
+                                $scope.select = select;
+                            };        
+            
             console.log($scope.itemsInventory);
         });
 
