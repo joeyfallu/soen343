b79cd3812d375959b41bf664a6056a97f5b2d1fd
Zachary Kogan <zackkogan@hotmail.com>
2017-11-08 14:34:49 -0500

    Adding test classes
    Added Purchase Mapper Test class
    #93

---
+++ src/test/java/com/example/myapp/purchases/PurchaseMapperTest.java
@@ -0,0 +1,29 @@
+package com.example.myapp.purchases;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PurchaseMapperTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void purchase() throws Exception {
+    }
+
+    @Test
+    public void insertPurchaseHistory() throws Exception {
+    }
+
+    @Test
+    public void deletePurchaseHistory() throws Exception {
+    }
+
+    @Test
+    public void returnItem() throws Exception {
+    }
+
+}
