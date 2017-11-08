7faaf3c85a39a580f0c0e2d8764276bbd8af9321
Zachary Kogan <zackkogan@hotmail.com>
2017-11-08 14:36:11 -0500

    Adding test classes
    Added Point Of Sale Test class for #93

---
+++ src/test/java/com/example/myapp/PointOfSaleTest.java
@@ -0,0 +1,33 @@
+package com.example.myapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointOfSaleTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void startPurchase() throws Exception {
+    }
+
+    @Test
+    public void cancelPurchase() throws Exception {
+    }
+
+    @Test
+    public void addCartItem() throws Exception {
+    }
+
+    @Test
+    public void removeCartItem() throws Exception {
+    }
+
+    @Test
+    public void endPurchase() throws Exception {
+    }
+
+}
