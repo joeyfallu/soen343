655d86074a90ce2bb56b8be5dbff98c33a4712a9
Zachary Kogan <zackkogan@hotmail.com>
2017-11-07 20:12:37 -0500

    testing ProductCatalog
    added tests for ProductCatalog methods

--- src/test/java/com/example/myapp/productCatalog/ProductCatalogTest.java
+++ src/test/java/com/example/myapp/productCatalog/ProductCatalogTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.HashMap;
@@ -15,17 +16,35 @@ import java.util.Map;
 public class ProductCatalogTest {
 
     @Mock
-    private ProductMapper productMapper;
+    private ProductMapper productMapperMock;
     @Mock
-    private ProductCatalog productCatalog;
+    private ProductCatalog productCatalogMock;
     @Mock
-    private Product product;
+    private Product productMock;
     private Map<Integer, Product> map = new HashMap<>();
 
     @Before
     public void setUp() throws Exception {
-        map.put(1,product);
-        productCatalog = new ProductCatalog();
+        map.put(1,productMock);
+        productCatalogMock = new ProductCatalog();
+    }
+
+    @Test
+    public void addProduct(){
+        ProductCatalog.addProduct(0, productMock);
+        Mockito.verify(productMapperMock).insert(productMock);
+    }
+
+    @Test
+    public void deleteProduct(){
+        ProductCatalog.deleteProduct(0);
+        Mockito.verify(productMapperMock).delete(0);
+    }
+
+    @Test
+    public void modifyProduct(){
+        ProductCatalog.modifyProduct(0, productMock);
+        Mockito.verify(productMapperMock).update(productMock);
     }
 
 
