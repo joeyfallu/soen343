3d23aac7bb7dd1e0a5f7b3733a98250ec6dda069
Philippe Li <philo1196@gmail.com>
2017-11-12 17:30:33 -0500

    fix #130 fixed routing after user deleted

--- src/main/resources/static/js/purchaseHistory.js
+++ src/main/resources/static/js/purchaseHistory.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('app')
-    .controller('purchaseHistoryController', function ($scope, $http, $route, $cookies) {
+    .controller('purchaseHistoryController', function ($scope, $http, $route, $cookies, $location) {
         $scope.purchaseHistory = "";
         $scope.emptyHistory = false;
         $scope.message = "";
 
         $http.get("/get/purchaseHistory").then((res) => {
             $scope.purchaseHistory = res.data;
-            console.log($scope.purchaseHistory);
             if (angular.equals($scope.purchaseHistory, {})) {
                 $scope.emptyHistory = true;
             }
