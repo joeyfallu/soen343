3a68af455d7ad6ae898f20fa538217d5edb38f2c
Philippe Li <philo1196@gmail.com>
2017-11-18 17:42:37 -0500

    wip #150 catalogs now only display one item of each model

--- src/main/resources/static/js/catalog.js
+++ src/main/resources/static/js/catalog.js
@@ -2,8 +2,6 @@
 
 angular.module('app')
     .controller('catalogController', function ($scope,$http, $cookies,$location) {
-
-
         $scope.cartItems = "";
         $scope.itemsInventory = "";
         $scope.selectedItem = "";
@@ -11,51 +9,58 @@ angular.module('app')
 
         $scope.currentPage = $location.absUrl().replace("http://localhost:8080/catalog/","");
 
-        console.log($scope.currentPage);
-        var cartItemsArray = []
-        for(var key in $scope.cartItems){
+        let cartItemsArray = [];
+        for (let key in $scope.cartItems) {
             cartItemsArray.push($scope.cartItems[key]);
         }
-        $scope.cartItems = cartItemsArray;
 
+        $scope.cartItems = cartItemsArray;
         $scope.listOfOptions = ['Show All', 'Most Expensive', 'Least Expensive', 'Most Recent', 'Least Recent', 'Heaviest', 'Lightest', 'Brand (A-Z)', 'Brand (Z-A)'];
 
-    $http.get('/get/allCarts').then((res) => {
-
-        $scope.cartItems = res.data;
-
-            $http.get('/get/products').then((res) => {
-            $scope.itemsInventory = res.data;
-            console.log($scope.itemsInventory);
-            for(var key in $scope.cartItems){
-                console.log($scope.cartItems[key].cartProducts);
-                for(var cartItemId in $scope.cartItems[key].cartProducts){
-                    console.log(cartItemId);
-                    delete $scope.itemsInventory[cartItemId];
+        /* Checks if a product's model is already in a array of items */
+        function hasModelNumber(items, product) {
+            for (let i = 0; i < items.length; i++) {
+                if (product.discriminator === items[i].discriminator && product.model === items[i].model) {
+                    return true;
                 }
             }
+            return false;
+        }
 
-            var items = [];
-            for(var key in $scope.itemsInventory){
-                items.push($scope.itemsInventory[key]);
-            }
+        $scope.updateCatalog = function() {
+            $http.get('/get/allCarts').then((res) => {
+                $scope.cartItems = res.data;
 
-            $scope.itemsInventory = randomizer(items);
+                $http.get('/get/products').then((res) => {
+                    $scope.itemsInventory = res.data;
 
-        });
-    });
+                    /* Remove products that are in user carts */
+                    for (let key in $scope.cartItems) {
+                        for (let cartItemId in $scope.cartItems[key].cartProducts){
+                            delete $scope.itemsInventory[cartItemId];
+                        }
+                    }
+
+                    let items = [];
+                    for(let key in $scope.itemsInventory){
+                        if (!hasModelNumber(items, $scope.itemsInventory[key]))
+                            items.push($scope.itemsInventory[key]);
+                    }
+
+                    $scope.itemsInventory = randomizer(items);
+                });
+            });
+        };
+
+        $scope.updateCatalog();
 
         $scope.getIsAdmin = function(){
             try {
-                var selectedItem = $cookies.getObject("USERINFO");
+                let selectedItem = $cookies.getObject("USERINFO");
                 return selectedItem.isAdmin;
-                let x = $cookies.getObject("USERINFO");
-                return x.isAdmin;
             } catch (error) {
-                console.log("Nobody is logged in");
                 return 2;
             }
-
         };
 
         $scope.addToCart = function(serialNumber){
@@ -69,15 +74,16 @@ angular.module('app')
                     alert("There are too many items in the cart\nMax items is 7 this item was not added");
                 }
                 else{
-                    var expireDate = new Date();
+                    let expireDate = new Date();
                     //300 seconds * 1000 milliseconds
                     expireDate.setTime(expireDate.getTime() + (300*1000));
 
-                    $cookies.put(serialNumber, serialNumber, {'expires': expireDate})
+                    $cookies.put(serialNumber, serialNumber, {'expires': expireDate});
 
-                    for(var i = 0; i < $scope.itemsInventory.length; i++){
+                    for(let i = 0; i < $scope.itemsInventory.length; i++){
                         if($scope.itemsInventory[i].serialNumber == serialNumber){
                             $scope.itemsInventory.splice(i,1);
+                            $scope.updateCatalog();
                         }
                     }
                 }
@@ -85,7 +91,7 @@ angular.module('app')
                 console.log("ERROR:");
                 console.log(err);
             });
-        }
+        };
 
         function randomizer(array){
             var currentIndex = array.length, temporaryValue, randomIndex;
